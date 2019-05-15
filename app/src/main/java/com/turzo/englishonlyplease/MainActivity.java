a0973317cbd3b5384c1456dc5d2e6543757fb5a9
Turzo <turzoxpress@gmail.com>
2019-05-15 22:58:33 +0600

    2nd Commit

--- app/src/main/java/com/turzo/englishonlyplease/MainActivity.java
+++ app/src/main/java/com/turzo/englishonlyplease/MainActivity.java
@@ -2,12 +2,51 @@ package com.turzo.englishonlyplease;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.turzo.onlyenglish.OnlyEnglishChecker;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText input;
+    Button b1,b2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = (EditText)findViewById(R.id.input);
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String tempS = OnlyEnglishChecker.returnOnlyLeftEnglishPart(input.getText().toString(),MainActivity.this,true,"Please type only English letters");
+                input.setText(tempS);
+                Log.d("asfasffsf",tempS);
+
+
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String tempS = OnlyEnglishChecker.retunWholeEnglishPart(input.getText().toString(),MainActivity.this,true,"Please type only English letters");
+                input.setText(tempS);
+                Log.d("asfasffsf",tempS);
+            }
+        });
     }
 }
--- onlyenglish/src/main/java/com/turzo/onlyenglish/OnlyEnglishChecker.java
+++ onlyenglish/src/main/java/com/turzo/onlyenglish/OnlyEnglishChecker.java
@@ -1,14 +1,11 @@
 package com.turzo.onlyenglish;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class OnlyEnglishChecker {
 
-    public static int plus(int a, int b){
-
-        return a+b;
-    }
 
     //---------------- 01
     //-- Checking the provided string has any foreign character, if has then function will return only left english part of the string
@@ -21,72 +18,77 @@ public class OnlyEnglishChecker {
     public static String returnOnlyLeftEnglishPart(String st, Context ct, boolean toastOn,String txt){
 
         String tempS = "";
-        /*
+
         boolean tempFlag = true;
 
         for(int i=0; i<st.length(); i++){
 
-            if(st.charAt(i) != 'a' ||
-                    st.charAt(i) != 'b' ||
-                    st.charAt(i) != 'c' ||
-                    st.charAt(i) != 'd' ||
-                    st.charAt(i) != 'e' ||
-                    st.charAt(i) != 'f' ||
-                    st.charAt(i) != 'g' ||
-                    st.charAt(i) != 'h' ||
-                    st.charAt(i) != 'i' ||
-                    st.charAt(i) != 'j' ||
-                    st.charAt(i) != 'k' ||
-                    st.charAt(i) != 'l' ||
-                    st.charAt(i) != 'm' ||
-                    st.charAt(i) != 'n' ||
-                    st.charAt(i) != 'o' ||
-                    st.charAt(i) != 'p' ||
-                    st.charAt(i) != 'q' ||
-                    st.charAt(i) != 'r' ||
-                    st.charAt(i) != 's' ||
-                    st.charAt(i) != 't' ||
-                    st.charAt(i) != 'u' ||
-                    st.charAt(i) != 'v' ||
-                    st.charAt(i) != 'w' ||
-                    st.charAt(i) != 'x' ||
-                    st.charAt(i) != 'y' ||
-                    st.charAt(i) != 'z' ||
+            if(st.charAt(i) == 'a' ||
+                    st.charAt(i) == 'b' ||
+                    st.charAt(i) == 'c' ||
+                    st.charAt(i) == 'd' ||
+                    st.charAt(i) == 'e' ||
+                    st.charAt(i) == 'f' ||
+                    st.charAt(i) == 'g' ||
+                    st.charAt(i) == 'h' ||
+                    st.charAt(i) == 'i' ||
+                    st.charAt(i) == 'j' ||
+                    st.charAt(i) == 'k' ||
+                    st.charAt(i) == 'l' ||
+                    st.charAt(i) == 'm' ||
+                    st.charAt(i) == 'n' ||
+                    st.charAt(i) == 'o' ||
+                    st.charAt(i) == 'p' ||
+                    st.charAt(i) == 'q' ||
+                    st.charAt(i) == 'r' ||
+                    st.charAt(i) == 's' ||
+                    st.charAt(i) == 't' ||
+                    st.charAt(i) == 'u' ||
+                    st.charAt(i) == 'v' ||
+                    st.charAt(i) == 'w' ||
+                    st.charAt(i) == 'x' ||
+                    st.charAt(i) == 'y' ||
+                    st.charAt(i) == 'z' ||
 
-                    st.charAt(i) != 'A' ||
-                    st.charAt(i) != 'B' ||
-                    st.charAt(i) != 'C' ||
-                    st.charAt(i) != 'D' ||
-                    st.charAt(i) != 'E' ||
-                    st.charAt(i) != 'F' ||
-                    st.charAt(i) != 'G' ||
-                    st.charAt(i) != 'H' ||
-                    st.charAt(i) != 'I' ||
-                    st.charAt(i) != 'J' ||
-                    st.charAt(i) != 'K' ||
-                    st.charAt(i) != 'L' ||
-                    st.charAt(i) != 'M' ||
-                    st.charAt(i) != 'N' ||
-                    st.charAt(i) != 'O' ||
-                    st.charAt(i) != 'P' ||
-                    st.charAt(i) != 'Q' ||
-                    st.charAt(i) != 'R' ||
-                    st.charAt(i) != 'S' ||
-                    st.charAt(i) != 'T' ||
-                    st.charAt(i) != 'U' ||
-                    st.charAt(i) != 'V' ||
-                    st.charAt(i) != 'W' ||
-                    st.charAt(i) != 'X' ||
-                    st.charAt(i) != 'Y' ||
-                    st.charAt(i) != 'Z' ){
+                    st.charAt(i) == 'A' ||
+                    st.charAt(i) == 'B' ||
+                    st.charAt(i) == 'C' ||
+                    st.charAt(i) == 'D' ||
+                    st.charAt(i) == 'E' ||
+                    st.charAt(i) == 'F' ||
+                    st.charAt(i) == 'G' ||
+                    st.charAt(i) == 'H' ||
+                    st.charAt(i) == 'I' ||
+                    st.charAt(i) == 'J' ||
+                    st.charAt(i) == 'K' ||
+                    st.charAt(i) == 'L' ||
+                    st.charAt(i) == 'M' ||
+                    st.charAt(i) == 'N' ||
+                    st.charAt(i) == 'O' ||
+                    st.charAt(i) == 'P' ||
+                    st.charAt(i) == 'Q' ||
+                    st.charAt(i) == 'R' ||
+                    st.charAt(i) == 'S' ||
+                    st.charAt(i) == 'T' ||
+                    st.charAt(i) == 'U' ||
+                    st.charAt(i) == 'V' ||
+                    st.charAt(i) == 'W' ||
+                    st.charAt(i) == 'X' ||
+                    st.charAt(i) == 'Y' ||
+                    st.charAt(i) == 'Z' ){
+
+                tempS = tempS + String.valueOf(st.charAt(i));
+                tempFlag = true;
+
+
+
+
+            }else {
 
                 tempFlag = false;
                 break;
 
-            }else {
 
-                tempFlag = true;
-                tempS = tempS + st.charAt(i);
 
             }
         }
@@ -100,7 +102,7 @@ public class OnlyEnglishChecker {
             }
 
         }
-        */
+
 
 
         return tempS;
@@ -124,71 +126,79 @@ public class OnlyEnglishChecker {
 
         for(int i=0; i<st.length(); i++){
 
-            if(st.charAt(i) != 'a' ||
-                    st.charAt(i) != 'b' ||
-                    st.charAt(i) != 'c' ||
-                    st.charAt(i) != 'd' ||
-                    st.charAt(i) != 'e' ||
-                    st.charAt(i) != 'f' ||
-                    st.charAt(i) != 'g' ||
-                    st.charAt(i) != 'h' ||
-                    st.charAt(i) != 'i' ||
-                    st.charAt(i) != 'j' ||
-                    st.charAt(i) != 'k' ||
-                    st.charAt(i) != 'l' ||
-                    st.charAt(i) != 'm' ||
-                    st.charAt(i) != 'n' ||
-                    st.charAt(i) != 'o' ||
-                    st.charAt(i) != 'p' ||
-                    st.charAt(i) != 'q' ||
-                    st.charAt(i) != 'r' ||
-                    st.charAt(i) != 's' ||
-                    st.charAt(i) != 't' ||
-                    st.charAt(i) != 'u' ||
-                    st.charAt(i) != 'v' ||
-                    st.charAt(i) != 'w' ||
-                    st.charAt(i) != 'x' ||
-                    st.charAt(i) != 'y' ||
-                    st.charAt(i) != 'z' ||
+            
+            if(st.charAt(i) == 'a' ||
+                    st.charAt(i) == 'b' ||
+                    st.charAt(i) == 'c' ||
+                    st.charAt(i) == 'd' ||
+                    st.charAt(i) == 'e' ||
+                    st.charAt(i) == 'f' ||
+                    st.charAt(i) == 'g' ||
+                    st.charAt(i) == 'h' ||
+                    st.charAt(i) == 'i' ||
+                    st.charAt(i) == 'j' ||
+                    st.charAt(i) == 'k' ||
+                    st.charAt(i) == 'l' ||
+                    st.charAt(i) == 'm' ||
+                    st.charAt(i) == 'n' ||
+                    st.charAt(i) == 'o' ||
+                    st.charAt(i) == 'p' ||
+                    st.charAt(i) == 'q' ||
+                    st.charAt(i) == 'r' ||
+                    st.charAt(i) == 's' ||
+                    st.charAt(i) == 't' ||
+                    st.charAt(i) == 'u' ||
+                    st.charAt(i) == 'v' ||
+                    st.charAt(i) == 'w' ||
+                    st.charAt(i) == 'x' ||
+                    st.charAt(i) == 'y' ||
+                    st.charAt(i) == 'z' ||
 
-                    st.charAt(i) != 'A' ||
-                    st.charAt(i) != 'B' ||
-                    st.charAt(i) != 'C' ||
-                    st.charAt(i) != 'D' ||
-                    st.charAt(i) != 'E' ||
-                    st.charAt(i) != 'F' ||
-                    st.charAt(i) != 'G' ||
-                    st.charAt(i) != 'H' ||
-                    st.charAt(i) != 'I' ||
-                    st.charAt(i) != 'J' ||
-                    st.charAt(i) != 'K' ||
-                    st.charAt(i) != 'L' ||
-                    st.charAt(i) != 'M' ||
-                    st.charAt(i) != 'N' ||
-                    st.charAt(i) != 'O' ||
-                    st.charAt(i) != 'P' ||
-                    st.charAt(i) != 'Q' ||
-                    st.charAt(i) != 'R' ||
-                    st.charAt(i) != 'S' ||
-                    st.charAt(i) != 'T' ||
-                    st.charAt(i) != 'U' ||
-                    st.charAt(i) != 'V' ||
-                    st.charAt(i) != 'W' ||
-                    st.charAt(i) != 'X' ||
-                    st.charAt(i) != 'Y' ||
-                    st.charAt(i) != 'Z' ){
-
-                tempFlag = false;
-                //break;
-
-            }else {
+                    st.charAt(i) == 'A' ||
+                    st.charAt(i) == 'B' ||
+                    st.charAt(i) == 'C' ||
+                    st.charAt(i) == 'D' ||
+                    st.charAt(i) == 'E' ||
+                    st.charAt(i) == 'F' ||
+                    st.charAt(i) == 'G' ||
+                    st.charAt(i) == 'H' ||
+                    st.charAt(i) == 'I' ||
+                    st.charAt(i) == 'J' ||
+                    st.charAt(i) == 'K' ||
+                    st.charAt(i) == 'L' ||
+                    st.charAt(i) == 'M' ||
+                    st.charAt(i) == 'N' ||
+                    st.charAt(i) == 'O' ||
+                    st.charAt(i) == 'P' ||
+                    st.charAt(i) == 'Q' ||
+                    st.charAt(i) == 'R' ||
+                    st.charAt(i) == 'S' ||
+                    st.charAt(i) == 'T' ||
+                    st.charAt(i) == 'U' ||
+                    st.charAt(i) == 'V' ||
+                    st.charAt(i) == 'W' ||
+                    st.charAt(i) == 'X' ||
+                    st.charAt(i) == 'Y' ||
+                    st.charAt(i) == 'Z' ){
 
                 tempFlag = true;
 
 
-            }
+                //Log.d("asfasffsf",String.valueOf(st.charAt(i)));
+                tempS = tempS + String.valueOf(st.charAt(i));
+                //break;
 
-            tempS = tempS + st.charAt(i);
+            }else {
+                tempFlag = false;
+
+
+
+
+            }
+            
+
+
+
         }
 
         //-- if the string contains foreign character then it will show the warning
